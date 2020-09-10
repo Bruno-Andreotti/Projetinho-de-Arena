b1c4c4ea2122536387fddb0568a7c3287f43152e
Bruno Andreotti <bruno.andreotti@me.com>
2020-09-09 22:38:56 -0300

    teste git

---
+++ TestPUN/Assets/Scripts/MyLobby.cs
@@ -0,0 +1,55 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLobby : MonoBehaviourPunCallbacks
+{
+    public string PlayerName;
+    public GameObject roomPanel;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MainGame()
+    {
+
+        PhotonNetwork.LocalPlayer.NickName = PlayerName;
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnConnectedToMaster()
+    {
+        roomPanel.SetActive(true);
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Não encontrou sala. Criando nova...");
+        string roomName = "Sala00";
+        RoomOptions r0p = new RoomOptions();
+        r0p.MaxPlayers = 20;
+        PhotonNetwork.CreateRoom(roomName, r0p);
+
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Level1");
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("Jogador " + PlayerName + " entrou na sala");
+    }
+}
---
+++ TestPUN/Assets/Scripts/Weapons.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public PhotonView pview;
+    public GameObject pointoffire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pview.IsMine)
+        {
+            if(Input.GetButton("Jump"))
+            {
+                PhotonNetwork.Instantiate("Bullet", pointoffire.transform.position, pointoffire.transform.rotation, 0);
+            }
+        }
+    }
+}
